7af086dbef2b4ed51e00c4ea477d803261d5b659
Michael King <1747304+tsal@users.noreply.github.com>
2024-09-29 12:13:59 -0400

    fix: add paradigm ui requirements

--- config/core.hpp
+++ config/core.hpp
@@ -1,9 +1,13 @@
+#include "paradigm.hpp"
+
+// TODO: move to functions.hpp
 class CfgFunctions {
 	#include "..\paradigm\client\functions.hpp"
 	#include "..\paradigm\server\functions.hpp"
 	#include "..\FirstCAV\functions.hpp"
 }
 
-class RscTitles : ParadigmRscTitles {
-	#include "..\FirstCAV\dialogs.hpp"
-}
+#include "ui\firstcav.hpp"
+
+// load profile namespace variables for runtime use
+__EXEC(allProfileNamespaceVars = allVariables profileNamespace);
