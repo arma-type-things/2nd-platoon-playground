0b59f8c2840bd6d6e323d7a5fcca3cead6033c94
Michael King <1747304+tsal@users.noreply.github.com>
2024-09-29 12:35:48 -0400

    feat: actually load paradigm init

--- config/core.hpp
+++ config/core.hpp
@@ -1,12 +1,5 @@
 #include "paradigm.hpp"
-
-// TODO: move to functions.hpp
-class CfgFunctions {
-	#include "..\paradigm\client\functions.hpp"
-	#include "..\paradigm\server\functions.hpp"
-	#include "..\FirstCAV\functions.hpp"
-}
-
+#include "functions.hpp"
 #include "ui\firstcav.hpp"
 
 // load profile namespace variables for runtime use
